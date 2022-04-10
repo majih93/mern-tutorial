df154b40500244602689232c2d35be74711b5dc6
Ji Hyuk Ma <t90laser93@gmail.com>
2022-04-11 01:02:37 +0900

    feat: 백엔드/몽고DB 연결
    - 몽고DB 세팅 완료
    - 몽고DB에 app 연결
    - 기본적인 CRUD 상호작용 체크 완료(POSTMAN)

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,14 @@
 // async handler library
 const asyncHandler = require("express-async-handler");
 
+const Goal = require("../models/goalModel");
+
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "get goals" });
+  const goals = await Goal.find();
+  res.status(200).json(goals);
 });
 
 // @desc    Set New goals
@@ -16,21 +19,46 @@ const setGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please add a text field");
   }
-  res.status(200).json({ message: "set goal" });
+
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+
+  res.status(200).json(goal);
 });
 
 // @desc    Update certain goal
 // @route   GET /api/goals/:id
 // @access  Private
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal Not Found");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedGoal);
 });
 
 // @desc    Delete certain goals
 // @route   GET /api/goals/:id
 // @access  Private
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal Not Found");
+  }
+
+  await goal.remove();
+
+  res.status(200).json({ id: req.params.id });
 });
 
 // 모듈 사용할 수 있도록 export
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 // entry point to server
-
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 
 // 환경변수 활용해서 PORT 값 설정, 안될 경우 대비해서 or 5000
 const port = process.env.PORT;
+
+// db 연결하는 함수 실행
+connectDB();
 
 // initialize express
 const app = express();
