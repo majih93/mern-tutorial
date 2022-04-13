0e83f4cc6be48988392d93c4f7cba05750e1fd16
Ji Hyuk Ma <t90laser93@gmail.com>
2022-04-13 18:18:54 +0900

    feat: 백엔드 완성 및 프론트 구축 시작
    백엔드
    - 로직 구현 완료(CRUD 및 jwt 인증)
    
    프론트엔드
    - 리액트 프로젝트 시작
    - 로그인/회원가입 UI 구현
    - 라우팅 설정

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -8,8 +8,10 @@ const {
   deleteGoal,
 } = require("../controllers/goalController");
 
-router.route("/").get(getGoals).post(setGoal);
-router.route("/:id").delete(deleteGoal).put(updateGoal);
+const { protect } = require("../middleware/authMiddleware");
+
+router.route("/").get(protect, getGoals).post(protect, setGoal);
+router.route("/:id").delete(protect, deleteGoal).put(protect, updateGoal);
 
 // Update matching id goal
 router.put("/:id", updateGoal);
